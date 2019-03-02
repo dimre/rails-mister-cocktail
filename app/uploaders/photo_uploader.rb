4cafdfce3df838368aaf3d8cc761af478da1029d
dimitri reisinger <dimitri.reisinger@hotmail.com>
2019-03-02 16:35:25 +0100

    opacity cards

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -5,4 +5,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
     resize_to_fit 100, 100
     cloudinary_transformation radius: 10
   end
+
+  version :show do
+    resize_to_fit 200, 200
+    cloudinary_transformation radius: 10
+  end
 end
